e0a5dab64d63b20208812becf141b1b3717a4c0d
MateoJJaramilloS <mateojaramillosalinas@gmail.com>
2026-01-28 11:58:34 -0500

    1

--- ManejoArchivos/src/lectura01/LeerArchivoTexto.java
+++ ManejoArchivos/src/lectura01/LeerArchivoTexto.java
@@ -71,8 +71,8 @@ public class LeerArchivoTexto {
                 String linea = entrada.nextLine();
                 List<String> lista = Arrays.asList(linea.split(";"));
                 ArrayList<String> linea_partes = new ArrayList<>(lista);
-                // for (int i = 0; i < linea_partes.size(); i++) {
-                    System.out.println(linea_partes.get(2));
+                for (int i = 0; i < linea_partes.size(); i++) {
+                    System.out.println(linea_partes.get(i));
                 // }
                 System.out.println("-----------------------------------");
 
